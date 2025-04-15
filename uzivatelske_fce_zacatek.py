f8c375d2bfbca32a766a8e912694e652089bfa39
dejnah <168757297+dejnah@users.noreply.github.com>
2025-04-15 14:16:11 +0200

    L7 Karel cvíčo uzivatelske_fce_zacatek

---
+++ uzivatelske_fce_zacatek.py
@@ -0,0 +1,20 @@
+### CVIČENÍ, Vyzkoušej si práci s *uživatelskými funkcemi*:
+# 1. Nahraj knihovnu `random`,
+# 2. vytvoř definici uživatelské funkce `vygeneruj_tuple`,
+# 3. funkce potřebuješ zadat **dva celočíselné parametry** `delka` a `max_hodnota`,
+# 4. funkce vrací datový typ `tuple`,
+# 5. obecným účelem funkce je vrátit takový *tuple*, který má délku podle `delka` a rozsah náhodných celých čísel je z intervalu `0 ~ max_hodnota`,
+# 6. nakonec funkci zavolej pro tyto argumenty: `5, 50`, `10, 100` a `15, 150` (pro parametry `delka`, `max_hodnota`).
+
+
+from random import choices
+
+def vygeneruj_tuple(delka, max_hodnota):
+    return tuple(choices(range(max_hodnota), k=delka))
+
+print(
+    vygeneruj_tuple(5, 50),
+    vygeneruj_tuple(10, 100),
+    vygeneruj_tuple(15, 150),
+    sep="\n"
+)
